a7700d433d9c04dda577f7d8f7a31924a2cb0262
DESKTOP-QRMT5EL\USER <dbwls_0811@naver.com>
2020-11-03 18:28:00 +0900

    dark

--- js/index.js
+++ js/index.js
@@ -1,12 +1,4 @@
 $(function() {
-    
-
-
-    //       $switch-thumb: grey;
-// $switch-thumb--active: #83b154;
-// $switch-track: #ccc;
-// $switch-track--active: #d0efa8;
-// fullpage
 var myFullpage = new fullpage('#fullpage', {
     anchors: ['home', 'aboutMe', 'projects', 'letsTalk'],
     css3: true,
@@ -17,12 +9,6 @@ var myFullpage = new fullpage('#fullpage', {
   // nav
   $('#gnb').on('click',function(){
     $('nav').toggleClass('on');
-
-
-//       $switch-thumb: grey;
-// $switch-thumb--active: #83b154;
-// $switch-track: #ccc;
-// $switch-track--active: #d0efa8;
   });
 
   // 모바일 화면에서 햄버거 메뉴 클릭 시 내비게이션이 나타나는 애니메이션
@@ -44,4 +30,22 @@ wrapperMenu.addEventListener('click', function () {
         
     }
 })
+
+// mode_btn
+$('.mode_box').on('click', function(){
+    //.addClass()와 .removeClass()가 순차적으로 일어나는 매서드
+    $('.mode_box').toggleClass('active');
+    $('.mode_btn').toggleClass('active');
+    $("body").css({"background":"#1d1d1d","color":"#fff"});
+    $("header").css({"background":"#1d1d1d","color":"#fff"});
+    $(".line-menu").css({"background":"#fff"});
+    $(".mobile_menu").css({"background":"#1d1d1d"});
+    $('.about_event .fp-controlArrow.fp-prev').toggleClass('active');
+    $('.about_event .fp-controlArrow.fp-next').toggleClass('active');
+    $(".scrollable-element > div").css({"background":"#fff"});
+    $('#call1').toggleClass('active');
+    $('#call2').toggleClass('active');
+    $('#call3').toggleClass('active');
+  });
+
 });
