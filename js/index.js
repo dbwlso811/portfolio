e86e26c779a4500dbaff8906d2ff12d764ab735a
DESKTOP-QRMT5EL\USER <dbwls_0811@naver.com>
2020-11-03 21:22:06 +0900

    menu

--- js/index.js
+++ js/index.js
@@ -6,24 +6,6 @@ var myFullpage = new fullpage('#fullpage', {
     normalScrollElements: '.scrollable-element',
   });
 
-
-  // mode_btn
-$('.mode_box').on('click', function(){
-  //.addClass()와 .removeClass()가 순차적으로 일어나는 매서드
-  $('body').toggleClass('active');
-  $('#header').toggleClass('active');
-  $('.mobile_menu').toggleClass('active');
-  $('.mode_box').toggleClass('active');
-  $('.mode_btn').toggleClass('active');
-  $('.mobile_nav .line-menu').toggleClass('active');
-  $('.fp-controlArrow.fp-prev').toggleClass('active');
-  $('.fp-controlArrow.fp-next').toggleClass('active');
-  $('.scrollable-element > div').toggleClass('active');
-  $('#call1').toggleClass('active');
-  $('#call2').toggleClass('active');
-  $('#call3').toggleClass('active');
-});
-
   // nav
   $('#gnb').on('click',function(){
     $('nav').toggleClass('on');
@@ -42,10 +24,33 @@ wrapperMenu.addEventListener('click', function () {
     } else {
         wrapperMenu.classList.remove('open');
         $('.mobile_menu').animate({ right: '-100%' }, 1000, 'easeOutCubic').removeClass('show');
-        $('header h1').removeClass('active');
-        
+        $('header h1').removeClass('active');    
     }
 })
 
+// 메뉴 리스트 클릭시 메뉴 사라짐
+$('.mobile_menu a').on('click', function(){
+  // $('header h1').hasClass('active');
+  $('.wrapper-menu').removeClass('open');
+  $('.mobile_menu').animate({ right: '-100%' }, 1000, 'easeOutCubic').removeClass('show');
+  $('header h1').removeClass('active');
+});
+
+  // mode_btn
+  $('.mode_box').on('click', function(){
+    //.addClass()와 .removeClass()가 순차적으로 일어나는 매서드
+    $('body').toggleClass('active');
+    $('#header').toggleClass('active');
+    $('.mobile_menu').toggleClass('active');
+    $('.mode_box').toggleClass('active');
+    $('.mode_btn').toggleClass('active');
+    $('.mobile_nav .line-menu').toggleClass('active');
+    $('.fp-controlArrow.fp-prev').toggleClass('active');
+    $('.fp-controlArrow.fp-next').toggleClass('active');
+    $('.scrollable-element > div').toggleClass('active');
+    $('#call1').toggleClass('active');
+    $('#call2').toggleClass('active');
+    $('#call3').toggleClass('active');
+  });
 
 });
