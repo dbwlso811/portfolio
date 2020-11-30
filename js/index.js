9473992e63590cc8cdf66ceab5aef1588aeab3db
DESKTOP-QRMT5EL\USER <dbwls_0811@naver.com>
2020-11-30 21:41:06 +0900

    netflix

--- js/index.js
+++ js/index.js
@@ -54,4 +54,62 @@ $('.mobile_menu a').on('click', function(){
     $('#call2').toggleClass('active');
     $('#call3').toggleClass('active');
   });
+  // call icon클릭시 number box 나타남
+  $('.call_icon').on('click', function(){
+    $('.console-container').toggleClass('active');
+  });
+
+
+
+  // call 아이콘 클릭시 번호창
+  consoleText(['010-8253-7035', 'Call me.'], 'number',['#1D77FF','#2e405c']);
+
+  function consoleText(words, id, colors) {
+    if (colors === undefined) colors = ['#fff'];
+    var visible = true;
+    var con = document.getElementById('console')  ;
+    var letterCount = 1;
+    var x = 1;
+    var waiting = false;
+    var target = document.getElementById(id)
+    target.setAttribute('style', 'color:' +   colors[0])
+    window.setInterval(function() {
+
+      if (letterCount === 0 && waiting ===  false) {
+        waiting = true;
+        target.innerHTML = words[0].substring (0, letterCount)
+        window.setTimeout(function() {
+          var usedColor = colors.shift();
+          colors.push(usedColor);
+          var usedWord = words.shift();
+          words.push(usedWord);
+          x = 1;
+          target.setAttribute('style', 'color:'   + colors[0])
+          letterCount += x;
+          waiting = false;
+        }, 1000)
+      } else if (letterCount === words[0].  length + 1 && waiting === false) {
+        waiting = true;
+        window.setTimeout(function() {
+          x = -1;
+          letterCount += x;
+          waiting = false;
+        }, 1000)
+      } else if (waiting === false) {
+        target.innerHTML = words[0].substring (0, letterCount)
+        letterCount += x;
+      }
+    }, 120)
+    window.setInterval(function() {
+      if (visible === true) {
+        con.className = 'console-underscore   hidden'
+        visible = false;
+
+      } else {
+        con.className = 'console-underscore'
+
+        visible = true;
+      }
+    }, 400)
+  }
 });
